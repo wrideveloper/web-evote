23525ae20975166d204b3a0a4c1bc1a0d205636c
user <brigas29@gmail.com>
2021-10-02 14:05:59 +0700

    ui Hasil Pemilihan

--- src/modules/LoggedIn/admin/HasilPemilihan.jsx
+++ src/modules/LoggedIn/admin/HasilPemilihan.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import { Table } from 'reactstrap';
 
-const HasilPemilihan = () => {
-    return (
-        <div>
-            <h1>Hasil Pemilihan</h1>
-        </div>
-    )
+const HasilPemilihan = (props) => {
+return (
+    <div><h1>Daftar Hasil pemilihan</h1>
+    
+    <Table striped>
+    <thead>
+        <tr>
+        <th>ID</th>
+        <th>Nama</th>
+        <th>Kelas</th>
+        <th></th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+        <th scope="row">1</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        </tr>
+        <tr>
+        <th scope="row">2</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        </tr>
+        <tr>
+        <th scope="row">3</th>
+        <td></td>
+        <td></td>
+        <td></td>
+        </tr>
+    </tbody>
+    </Table>
+    </div>
+);
 }
 
-export default HasilPemilihan
+export default HasilPemilihan;
