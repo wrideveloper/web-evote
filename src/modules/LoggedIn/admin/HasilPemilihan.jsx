60a924b7b4e76f5c8eba391c56f36bfe2ebc7709
Ahmad Rafif Alaudin <rafifayuzaki123@gmail.com>
2021-10-03 16:37:44 +0700

    Implement ui card harapan dan saran

--- src/modules/LoggedIn/admin/HasilPemilihan.jsx
+++ src/modules/LoggedIn/admin/HasilPemilihan.jsx
@@ -1,8 +1,5 @@
 import React from 'react'
-import {
-    Card, CardImg, CardText, CardBody,
-    CardTitle, CardSubtitle, Button
-} from 'reactstrap';
+import CardHarapan from '../../../components/Card/CardHarapan'
 
 const HasilPemilihan = () => {
     return (
@@ -16,29 +13,7 @@ const HasilPemilihan = () => {
     )
 }
 
-const CardHarapan = (props) => {
-    return (
-        <div>
-            <Card style={{ backgroundColor: '#f2f2f2', width: '85%', margin: 'auto', border: 'none', borderRadius: '8px' }}>
-                <CardBody>
-                    <div style={{ padding: '10px' }}>
-                        <div style={{ paddingBottom: '30px' }}>
-                            <CardTitle tag="h4" style={{ fontWeight: 'bold', color: '#c58f17' }}>{props.name}</CardTitle>
-                        </div>
-                        <div style={{ paddingBottom: '40px' }}>
-                            <CardSubtitle tag="h6" style={{ fontWeight: 'bold' }}>Harapan</CardSubtitle>
-                            <CardText style={{ lineHeight: '1.8' }}>{props.harapan}</CardText>
-                        </div>
-                        <div style={{ paddingBottom: '20px' }}>
-                            <CardSubtitle tag="h6" style={{ fontWeight: 'bold' }}>Saran</CardSubtitle>
-                            <CardText style={{ lineHeight: '1.8' }}>{props.saran}</CardText>
-                        </div>
-                    </div>
-                </CardBody>
-            </Card>
-        </div>
-    );
-};
+
 
 
 export default HasilPemilihan
