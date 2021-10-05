37d10620b43483ec1d1891c44766495beb78954b
Milayn <millayunita07@gmail.com>
2021-10-05 07:49:55 +0700

    e-vote Pemilih MY

--- src/modules/LoggedIn/admin/Pemilih.jsx
+++ src/modules/LoggedIn/admin/Pemilih.jsx
@@ -1,10 +1,102 @@
 import React from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEdit, faTrash, faEye } from "@fortawesome/free-solid-svg-icons";
+import { Table, Button } from 'reactstrap';
 
 const Pemilih = () => {
     return (
         <div>
-            <h1>Pemilih</h1>
+            <Button color="secondary" style={{marginLeft:'40px'}}>
+            Tambah Pemilih
+            </Button>
+            <br /><br />
+      <Table striped style={{width:'1025px',marginLeft:'40px', marginTop:'5px', marginRight:'20px',cellpadding:'10', cellspacing:'5', border:'1', borderColor:'lightgray'}}>
+      
+      <thead>
+        <tr style={{textAlign: 'center',cellpadding:'10px'}}>
+          <th style={{width:'205px', fontSize:'18px'}}>Nim</th>
+          <th style={{width:'400px', fontSize:'18px'}}>Nama</th>
+          <th style={{width:'170px', fontSize:'18px'}}>Kelas</th>
+          <th style={{width:'250px', fontSize:'18px'}}>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope="row"></th>
+          <td></td>
+          <td></td>
+          <td>
+            <div style={{display: 'flex', flexdirection:'row',justifyContent:'center',padding:'7px'}}>
+              <div style={{margin:'2px'}}>
+                <Button color="primary">
+                <FontAwesomeIcon icon={faEye}/>
+                </Button>
+              </div>
+              <div  style={{margin:'2px'}}>
+                <Button color="danger">
+                <FontAwesomeIcon icon={faTrash} />
+                </Button>
+              </div>
+              <div style={{margin:'2px'}}>
+                <Button color="warning">
+                <FontAwesomeIcon icon={faEdit} />
+                </Button>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row"></th>
+          <td></td>
+          <td></td>
+          <td>
+          <div style={{display: 'flex', flexdirection:'row',justifyContent:'center',padding:'7px'}}>
+              <div style={{margin:'2px'}}>
+                <Button color="primary">
+                <FontAwesomeIcon icon={faEye}/>
+                </Button>
+              </div>
+              <div  style={{margin:'2px'}}>
+                <Button color="danger">
+                <FontAwesomeIcon icon={faTrash} />
+                </Button>
+              </div>
+              <div style={{margin:'2px'}}>
+                <Button color="warning">
+                <FontAwesomeIcon icon={faEdit} />
+                </Button>
+              </div>
+            </div>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row"></th>
+          <td></td>
+          <td></td>
+          <td>
+          <div style={{display: 'flex', flexdirection:'row',justifyContent:'center',padding:'7px'}}>
+              <div style={{margin:'2px'}}>
+                <Button color="primary">
+                <FontAwesomeIcon icon={faEye}/>
+                </Button>
+              </div>
+              <div  style={{margin:'2px'}}>
+                <Button color="danger">
+                <FontAwesomeIcon icon={faTrash} />
+                </Button>
+              </div>
+              <div style={{margin:'2px'}}>
+                <Button color="warning">
+                <FontAwesomeIcon icon={faEdit} />
+                </Button>
+              </div>
+            </div>
+          </td>
+        </tr>
+      </tbody>
+    </Table>
         </div>
+
     )
 }
 
