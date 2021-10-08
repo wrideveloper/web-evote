e28b6da7243749afc8bc134216291727ae35ad3d
Rosandymaulana <rosandy1118@gmail.com>
2021-10-08 13:18:59 +0700

    Diagram Pie

--- src/modules/LoggedIn/admin/HasilPemilihan.jsx
+++ src/modules/LoggedIn/admin/HasilPemilihan.jsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
+import '../../../components/Pemilihan.css';
+
+const Deskripsi = (props) => {
+        return(
+            <Col className="contents" sm="5">
+                <h1 className="total-suara">{props.jumlah}</h1>
+                <div className="deskripsi">
+                    <p>Total - Suara</p>
+                    <p className="calon">{props.calon}</p>
+                </div>
+            </Col>
+        )
+}
+
+const TotalSuara = (props) => {
+    return(
+        <h3  className="total-suara-final">Total Suara {props.TotalSuara}</h3>
+    )
+}
 
 const HasilPemilihan = () => {
+
+    
+
     return (
-        <div>
-            <h1>Hasil Pemilihan</h1>
-        </div>
+        <Container>
+            <Row className="box">
+                <Col xs="7">
+                    <div className="paragraf">
+                        <Row>
+                            <Deskripsi jumlah="59" calon="Calon 1"></Deskripsi>
+                            <Deskripsi jumlah="26" calon="Calon 2"></Deskripsi>
+                        </Row>                  
+                    </div>  
+                </Col>
+
+                <Col className="pie" xs="5">
+                    <h1>Diagram Pie</h1>    
+                </Col>
+                <TotalSuara TotalSuara="85"></TotalSuara>
+            </Row>
+        </Container>
     )
 }
 
