e37ebcef055354a44627e2920e4badd898ec2bec
Rosandymaulana <rosandy1118@gmail.com>
2021-10-08 16:30:28 +0700

    implement UI for point hasil pemilihan

--- src/modules/LoggedIn/admin/HasilPemilihan.jsx
+++ src/modules/LoggedIn/admin/HasilPemilihan.jsx
@@ -1,37 +1,17 @@
 import React from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import '../../../components/Pemilihan.css';
-
-const Deskripsi = (props) => {
-        return(
-            <Col className="contents" sm="5">
-                <h1 className="total-suara">{props.jumlah}</h1>
-                <div className="deskripsi">
-                    <p>Total - Suara</p>
-                    <p className="calon">{props.calon}</p>
-                </div>
-            </Col>
-        )
-}
-
-const TotalSuara = (props) => {
-    return(
-        <h3  className="total-suara-final">Total Suara {props.TotalSuara}</h3>
-    )
-}
+import DeskripsiHasilPemilihan from '../../../components/DeskripsiHasilPemilihan';
 
 const HasilPemilihan = () => {
-
-    
-
-    return (
+  return (
         <Container>
             <Row className="box">
                 <Col xs="7">
                     <div className="paragraf">
                         <Row>
-                            <Deskripsi jumlah="59" calon="Calon 1"></Deskripsi>
-                            <Deskripsi jumlah="26" calon="Calon 2"></Deskripsi>
+                            <DeskripsiHasilPemilihan jumlah="59" calon="Calon 1"/>
+                            <DeskripsiHasilPemilihan jumlah="26" calon="Calon 2"/>
                         </Row>                  
                     </div>  
                 </Col>
@@ -39,7 +19,7 @@ const HasilPemilihan = () => {
                 <Col className="pie" xs="5">
                     <h1>Diagram Pie</h1>    
                 </Col>
-                <TotalSuara TotalSuara="85"></TotalSuara>
+                <h3 className="total-suara-final">Total Suara : 85</h3>
             </Row>
         </Container>
     )
