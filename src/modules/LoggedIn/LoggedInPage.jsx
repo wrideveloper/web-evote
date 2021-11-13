6ed7c43627b2cd6255b79a7d3a75a5590d77d41c
Rosandymaulana <rosandy1118@gmail.com>
2021-11-13 22:12:31 +0700

    Tugas Maulana dan Brigas

--- src/modules/LoggedIn/LoggedInPage.jsx
+++ src/modules/LoggedIn/LoggedInPage.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
 // import Dashboard from './admin/Dashboard'
 import Home from './user/Home'
-
 import Header from '../../components/header'
 import Footer from '../../components/footer'
+import Harapan from './user/Harapan'
 
 const LoggedInPage = () => {
     return (
         <div>
             <Header />
             <Switch>
-                <Route path="/evote" component={Home} />
+                <Route path="/evote" component={Harapan} />
                 <Redirect to="/evote" />
             </Switch>
             <Footer />
