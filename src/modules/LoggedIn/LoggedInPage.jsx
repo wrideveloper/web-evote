eda32c8fa331e39770d16231f6843f3f6540aae4
Milayn <millayunita07@gmail.com>
2021-11-13 23:55:25 +0700

    Update ProfilCaketum

--- src/modules/LoggedIn/LoggedInPage.jsx
+++ src/modules/LoggedIn/LoggedInPage.jsx
@@ -2,15 +2,17 @@ import React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
 // import Dashboard from './admin/Dashboard'
 import Home from './user/Home'
-
+import ProfilCaketum from './user/ProfilCaketum.jsx'
 import Header from '../../components/header'
 import Footer from '../../components/footer'
+
 
 const LoggedInPage = () => {
     return (
         <div>
             <Header />
             <Switch>
+                <Route path="/evote/profile" component={ProfilCaketum} />
                 <Route path="/evote" component={Home} />
                 <Redirect to="/evote" />
             </Switch>
--- src/modules/LoggedIn/user/Home.jsx
+++ src/modules/LoggedIn/user/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            <h1>Tempat Halaman home page</h1>
+
         </div>
     )
 }
