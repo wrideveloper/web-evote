135b317446affb14437d2b085c6bb4fcfb1272a9
naufalyukafi10@gmail.com <naufalyukafi10@gmail.com>
2021-11-14 22:44:22 +0700

    fix lagi

--- src/components/header.js
+++ src/components/header.js
@@ -1,33 +1,23 @@
-import React, { useState } from "react";
-import { Navbar, NavbarBrand, Collapse, NavbarToggler, Nav, NavItem, NavLink, NavbarText } from 'reactstrap'
+import React from "react";
 import { Link } from "react-router-dom";
 import "./header.css"
 
 function Header() {
-  const [isOpen, setIsOpen] = useState(false);
-  
-  const handleTogle = () => setIsOpen(prev => !prev)
-
   return (
-    <Navbar dark expand="md">
-       <NavbarBrand href="/evote">
-        <img className="logo" alt="logo" src={process.env.PUBLIC_URL + '/images/logo-wri-banner2.png'} />
-      </NavbarBrand>
-      <NavbarToggler onClick={handleTogle} style={{marginRight: 10}} />
-      <Collapse isOpen={isOpen} navbar>
-        <Nav className="flex m-md-auto" navbar>
-          <NavItem >
-            <NavLink href="#">Home</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="#" active>Lihathasil vote</NavLink>
-          </NavItem>
-          <NavItem >
-            <NavLink href="#">LogOut</NavLink>
-          </NavItem>
-        </Nav>
-      </Collapse>
-    </Navbar>
+    <nav className="navbar">
+      <img className="logo" alt="logo" src={process.env.PUBLIC_URL + '/images/logo-wri-banner2.png'} />
+      <ul className="nav-links">
+        <Link to="#" className="home">
+          <li>Home</li>
+        </Link>
+        <Link to="#" className="hasil">
+          <li>Lihat hasil vote</li>
+        </Link>
+        <Link to="#" className="logout">
+          <li>Log Out</li>
+        </Link>
+      </ul>
+    </nav>
   );
 }
 
