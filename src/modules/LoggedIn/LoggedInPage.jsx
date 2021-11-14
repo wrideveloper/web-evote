d6a9de40557b1202f1b12df1f65ff7d1734dde4f
naufalyukafi10@gmail.com <naufalyukafi10@gmail.com>
2021-11-14 22:53:13 +0700

    task milla dan rafif

--- src/modules/LoggedIn/LoggedInPage.jsx
+++ src/modules/LoggedIn/LoggedInPage.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import { Redirect, Route, Switch } from 'react-router'
 // import Dashboard from './admin/Dashboard'
 import Home from './user/Home'
+<<<<<<< HEAD
+=======
+import ProfilCaketum from './user/ProfilCaketum.jsx'
+>>>>>>> c7f6504cb532aa147d4f9adabd33a9e93f02b2ce
 import Header from '../../components/header'
 import Footer from '../../components/footer'
 import Harapan from './user/Harapan'
+
 
 const LoggedInPage = () => {
     return (
         <div>
             <Header />
             <Switch>
+<<<<<<< HEAD
                 <Route path="/evote" component={Harapan} />
+=======
+                <Route path="/evote/profile" component={ProfilCaketum} />
+                <Route path="/evote" component={Home} />
+>>>>>>> c7f6504cb532aa147d4f9adabd33a9e93f02b2ce
                 <Redirect to="/evote" />
             </Switch>
             <Footer />
--- src/modules/LoggedIn/user/Home.jsx
+++ src/modules/LoggedIn/user/Home.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 
 const Home = () => {
     return (
+<<<<<<< HEAD
         <div className="home-page">
             <h1>Tempat Halaman home page</h1>
+=======
+        <div>
+
+>>>>>>> c7f6504cb532aa147d4f9adabd33a9e93f02b2ce
         </div>
     )
 }
