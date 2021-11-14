46c346261956b2346107f4df6e1beeda75d26fce
naufalyukafi10@gmail.com <naufalyukafi10@gmail.com>
2021-11-14 21:59:45 +0700

    fix conflict local

--- src/modules/LoggedIn/user/Home.jsx
+++ src/modules/LoggedIn/user/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
+        <div className="home-page">
             <h1>Tempat Halaman home page</h1>
         </div>
     )
