6858828a12f5c55595aedf8f66e474b9481dec5f
Rosandymaulana <rosandy1118@gmail.com>
2021-11-28 19:01:15 +0700

    responsiveHarapan

--- src/modules/LoggedIn/user/Harapan.jsx
+++ src/modules/LoggedIn/user/Harapan.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from 'react'
-import { Button, Row, Col, Container, Card, CardBody, CardTitle, CardSubtitle, FormGroup, Label, Input , Table, CardFooter} from 'reactstrap';
+import { Button, Row, Col, Container, Card, CardBody, CardTitle, CardSubtitle, FormGroup, Label, Input , Table, CardFooter, Pagination} from 'reactstrap';
 import Picker from 'emoji-picker-react';
 import axios from 'axios';
+import { dateNumber } from '../../../helper/date';
+import '../../../components/responsiveHarapan.css';
 
 export default function Harapan() {
 
+    // console.log(dateNumber)
     const [inputStr, setInputstr] = useState('');
     const [showPicker, setShowPicker] = useState(false);
 
@@ -12,8 +15,6 @@ export default function Harapan() {
         setInputstr( prevInput => prevInput + emojiObject.emoji );
         setShowPicker(false);
     };
-
-    
 
     // const hitung = () => {
 
@@ -32,12 +33,34 @@ export default function Harapan() {
 
     async function getHarapan() {
         try {
-            const response = await axios.get('http://evote.ceban-app.com/calon');
+            const response = await axios.get('http://evote.ceban-app.com/vote');
             setHarapan(response.data)
-            //console.log("Halo ini Data Calon",response.data);
+            console.log("Halo ini Data Calon",response.data);
         } catch (error) {
             console.error(error);
         }
+    }
+
+    const [inputHarapan, setInputHarapan] = useState("")
+
+    async function postHarapan() {
+        try {
+            const response = await axios.post('http://evote.ceban-app.com/vote', {
+                id_calon: '3',
+                id_user: '3',
+                harapan: inputHarapan,
+                waktu_vote: dateNumber
+            });
+            alert('berhasil');
+            console.log(response);
+        }catch (error){
+            console.error(error);
+        }
+    }
+
+    const handleInputHarapan = (event) => {
+        setInputHarapan(event.target.value);
+        console.log(inputHarapan)
     }
 
     //console.log(getCalon);
@@ -48,13 +71,13 @@ export default function Harapan() {
         
     return (
         <div>
-            <center><h1 style={{marginTop: '83px'}}>Sampaikan Harapanmu</h1>
-            <h1 style={{ borderBottom: 'solid #f7b217 10px', width:'345px',borderRadius:'20px', margin:'20px 0px 60px 0px'}}></h1>
+            <center><h1 className="judul" style={{marginTop: '83px'}}>Sampaikan Harapanmu</h1>
+            <h1 style={{ borderBottom: 'solid #f7b217 10px', width:'23%',borderRadius:'20px', margin:'20px 0px 60px 0px'}}></h1>
             </center>
             <Container>
                 <Row>
-                    <Col xs="3" style={{textAlign:'center'}}>
-                        <img src='/images/logo.png' alt="" style={{width: '250px'}}/>
+                    <Col className="inputt" xs="3" style={{textAlign:'center'}}>
+                        <img className="log" src='/images/logo.png' alt="" style={{width: '70%'}}/>
                     </Col>
                     <Col >
                         <Card>
@@ -79,17 +102,19 @@ export default function Harapan() {
                             </CardSubtitle>
                                 <FormGroup>
                                     <Input
+                                    className="inputArea"
                                     id="my-textarea"
                                     name="text"
                                     type="textarea"
                                     maxlength="150" 
                                     placeholder= "Tulis Harapan" 
-                                    style={{textAlign: 'center', height: "230px" , padding: '100px', verticalAlign: 'middle'}}
+                                    style={{height: "230px" , padding: '5%', verticalAlign: 'middle'}}
+                                    onChange={(event) => handleInputHarapan(event)}
                                     />
                                     {/* <div className="my-textarea-remaining-chars">150 characters remaining</div> */}
                                 </FormGroup>
                                 {/* Emoticon */}
-                                <div className="picker-container" style={{textAlign: 'right', paddingTop: '40px'}}>
+                                {/* <div className="picker-container" style={{textAlign: 'right', paddingTop: '40px'}}>
                                     <input style={{width: '180px', height: '30px', borderRadius: '50px', padding: '20px', margin: '20px'}}
                                         className="input-style"
                                         value={inputStr}  
@@ -99,10 +124,10 @@ export default function Harapan() {
                                         {showPicker && <Picker 
                                             pickerStyle={{width: '100%'}}
                                             onEmojiClick={onEmojiClick} />}
-                                </div>
+                                </div> */}
                             </CardBody>                         
                             <CardFooter style={{padding: "45px 68px 45px 45px", textAlign: 'right'}}>
-                                <Button color="warning" style={{color: 'white', width: '150px', borderRadius: '100px'}}>
+                                <Button onClick={postHarapan} color="warning" style={{color: 'white', width: '150px', borderRadius: '100px'}}>
                                     Submit
                                 </Button>
                             </CardFooter>
@@ -113,9 +138,9 @@ export default function Harapan() {
             </Container>
             <br/><br/>
             <Container style={{marginBottom: '50px'}}>
-                 <Row>
+                 
                     <Card>
-                        <CardBody style={{marginLeft: '45px'}}>
+                        <CardBody style={{marginLeft: '5%', marginRight:'5%'}}>
                             <h5 style={{marginTop:'58px'}}><img src="/images/Rectangle 92 .png" alt="" style={{marginRight: '15px', width: '19px'}}></img>Harapan kami</h5>
                             <Table  style={{marginTop:'45px'}}>
                                 <thead >
@@ -126,16 +151,16 @@ export default function Harapan() {
                                     </tr>
                                 </thead>
                                         {
-                                            harapan.map((listCalon) => (
+                                            harapan.map((listHarapan) => (
                                                 <>
                                                 <tbody>
                                         <tr>
-                                        <td scope="row" style={{paddingTop:'65px', paddingRight: '60px', paddingBottom: '60px'}}>
-                                            {listCalon.nama}
-                                        </td>
-                                        
-                                        <td style={{paddingTop:'65px', paddingBottom: '60px'}}>
-                                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo, laudantium! Necessitatibus vero quos ducimus, quas optio ab nisi delectus dolores reiciendis, nobis ipsa voluptate quidem, deserunt molestiae mollitia fuga sequi?</td>
+                                        <td className="nama" scope="row" style={{paddingTop:'5%',  paddingBottom: '5%', fontWeight: 'bold'}}>
+                                            {listHarapan.nama_pemilih}
+                                        </td>                                       
+                                        <td className="nama" style={{paddingTop:'5%', paddingBottom: '5%'}}>
+                                            {listHarapan.harapan}</td>
+
                                         </tr>
                 
                                         {/* Fetching */}
@@ -147,7 +172,7 @@ export default function Harapan() {
                             </Table>
                         </CardBody>
                     </Card>
-                </Row>
+                
             </Container>
                 
                     
