9486dd845627ad5a055ff0828c341095214bf02d
dimadani <dikia656@gmail.com>
2021-12-01 20:37:38 +0700

    fixing homepage

--- src/modules/LoggedIn/user/Home/index.jsx
+++ src/modules/LoggedIn/user/Home/index.jsx
@@ -68,12 +68,13 @@ const Home = () => {
           backgroundImage: 'url("../images/home-image.png")',
           backgroundSize: "cover",
           backgroundRepeat: "no-repeat",
+          backgroundPosition: "center",
           display: "flex",
           justifyContent: "center",
           alignItems: "center",
           flexDirection: "column",
           minHeight: "100vh",
-          padding: "0 5px"
+          padding: "0 5px",
         }}
       >
         <div className="hero">
@@ -81,9 +82,7 @@ const Home = () => {
           <h4 className="desc">"Buatlah Perubahan Dengan Pilihanmu"</h4>
         </div>
       </div>
-      <Card
-        className="card__welcome"
-      >
+      <Card className="card__welcome">
         <CardBody
           style={{ borderLeft: "solid #f7b217 15px", padding: "20px 35px" }}
         >
