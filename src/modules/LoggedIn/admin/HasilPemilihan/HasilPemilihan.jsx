8fc1667cc05b7df685eb51cb30b03acf99ef0837
Rosandymaulana <rosandy1118@gmail.com>
2021-12-04 18:08:17 +0700

    delete error

--- src/modules/LoggedIn/admin/HasilPemilihan/HasilPemilihan.jsx
+++ src/modules/LoggedIn/admin/HasilPemilihan/HasilPemilihan.jsx
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { Container, Row, Col, Table, Button, CardBody, CardTitle, CardSubtitle, Card, CardText } from 'reactstrap';
+import { Container, Row, Col, Table, Button, CardBody, CardTitle, CardSubtitle, CardText } from 'reactstrap';
 import { Link } from 'react-router-dom'
 import '../../../../components/Pemilihan.css';
-import DeskripsiHasilPemilihan from '../../../../components/DeskripsiHasilPemilihan';
 import axios from 'axios';
 
 const HasilPemilihan = () => {
@@ -133,6 +132,7 @@ const HasilPemilihan = () => {
                         </Col>
                         <Col className="bg-light border" xs="4" tag="h2" style={{ marginTop: "5%" }}>
                             <center> Diagram Pie </center>
+                            {/*  */}
                         </Col>
                     </Row>
                     {
@@ -145,28 +145,6 @@ const HasilPemilihan = () => {
                 </Container>
             </Container>
 
-            {/* <Container>
-                <div className="text-end mb-3">
-                    <Link to="/admin/hasil-pemilihan/saran"><Button style={{ backgroundColor: '#3F3D56' }}>Harapan dan Saran</Button></Link>
-                </div>
-                <Row className="box">
-                    <Col xs="7">
-                        <div className="paragraf">
-                            <Row>
-                                <DeskripsiHasilPemilihan jumlah="59" calon="Calon 1" />
-                                <DeskripsiHasilPemilihan jumlah="26" calon="Calon 2" />
-                                <DeskripsiHasilPemilihan jumlah="26" calon="Calon 3" />
-                            </Row>
-                        </div>
-                    </Col>
-
-                    <Col className="pie" >
-                        <h1>Diagram Pie</h1>
-                    </Col>
-
-                    <h3 className="total-suara-final">Total Suara : 85</h3>
-                </Row>
-            </Container> */}
             <Table striped bordered hover className="mt-4">
                 <thead>
                     <tr>
