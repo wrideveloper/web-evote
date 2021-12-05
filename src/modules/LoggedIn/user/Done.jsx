394dcb678b5ffa444e8b228b54d5685ab3619818
Milayn <millayunita07@gmail.com>
2021-12-05 11:23:38 +0700

    Done Page TaskMila

--- src/modules/LoggedIn/user/Done.jsx
+++ src/modules/LoggedIn/user/Done.jsx
@@ -5,45 +5,47 @@ const Done = () => {
     return (
         <div>
             <div style={{ 
-                backgroundColor: "#FAFAFA",
-                width: "95%",
-                margin: "56px auto"
+                backgroundImage: `url("../images/bg-done.png")`,
+                backgroundSize: 'cover',
+                backgroundPosition: 'center center',
+                width: '94%', 
+                height: '100vh',
+                margin: '3%'
             }}>
                 <div style={{
                     textAlign: "center",
-                    color: '#7D7D7D'
+                    color: '#7D7D7D',
+                    display: "flex",
+                    flexDirection: "column"
+                    
                 }}>
                     <h4 style={{
-                        margin: '5px 0px',
+                        margin: '80vh 0px',
                         lineHeight: '46px',
                         letterSpacing: '0.0025em',
                         fontSize: '34px',
                         fontFamily: "Nunito Sans",
                         fontWeight: "normal"
-                    }}>Selamat!</h4>
-                    <h4 style={{
-                        margin: '5px 0px',
-                        lineHeight: '46px',
-                        letterSpacing: '0.0025em',
-                        fontSize: '34px',
-                        fontFamily: "Nunito Sans",
-                        fontWeight: "normal"
-                    }}>Votemu sudah masuk.</h4>
+                    }}>Selamat! <br />Votemu sudah masuk.</h4>
                 </div>
             </div>
             
-            <button style={{
-                width: '182px',
-                height: '62px',
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center'
+            }}>
+              <button style={{
+                width: '190px',
+                height: '60px',
                 backgroundColor: '#F7B217',
                 borderRadius: '50px',
                 color: 'white',
                 border: '0px',
                 marginBottom: '119px',
-                marginTop: '56px',
+                marginTop: '40px',
                 position: 'relative',
-                left: '44%'
-            }}>Tulis Harapan</button>
+                }}>Tulis Harapan</button>
+            </div>
 
         </div>
     );
